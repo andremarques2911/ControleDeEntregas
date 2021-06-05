43007d7011f38fa9c4a2a863811f281151e61976
André <Marques>
2021-06-05 05:46:15 -0300

    Adicionado testes de sistema

--- web_driver/Program.cs
+++ web_driver/Program.cs
@@ -5,21 +5,28 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace Login
 {
     class Program
     {
-        const string BROWSER = "CHROME";
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver(@"chrome\chromedriver");
+            // CreateOperatorTest();
+            RegisterWithdrawalTest();
+        }
 
+        static void CreateOperatorTest()
+        {
+            IWebDriver driver = new ChromeDriver(@"chrome\chromedriver");
             driver.Navigate().GoToUrl("http://localhost:3000/");
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             IWebElement firstResult = wait.Until(e => e.FindElement(By.Id("main_operators")));
             firstResult.Click();
-            var operatorsOld = driver.FindElements(By.CssSelector("[id^=operator_]"));
+            Thread.Sleep(2000);
+            IList<IWebElement> operatorsOld = driver.FindElements(By.CssSelector("[id^=operator_]"));
             Thread.Sleep(2000);
             driver.FindElement(By.Id("first_name")).SendKeys("Gui");
             Thread.Sleep(2000);
@@ -27,15 +34,33 @@ namespace Login
             Thread.Sleep(2000);
             driver.FindElement(By.Id("save_operator")).Click();
             Thread.Sleep(2000);
-            // swal2-title
-            // Thread.Sleep(1000);
             driver.FindElement(By.CssSelector(".swal2-confirm")).Click();
-            // driver.FindElement(By.Id("swal2-confirm")).Click();
             Thread.Sleep(2000);
-            var operatorsNew = driver.FindElements(By.CssSelector("[id^=operator_]"));
-            
+            IList<IWebElement> operatorsNew = driver.FindElements(By.CssSelector("[id^=operator_]"));
+            Assert.AreEqual(operatorsOld.Count + 1, operatorsNew.Count);
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
 
-            // driver.Quit();
+        static void RegisterWithdrawalTest() {
+            IWebDriver driver = new ChromeDriver(@"chrome\chromedriver");
+            driver.Navigate().GoToUrl("http://localhost:3000/");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement firstResult = wait.Until(e => e.FindElement(By.Id("main_deliveries")));
+            firstResult.Click();
+            IList<IWebElement> deliveriesOld = wait.Until(e => e.FindElements(By.CssSelector("[id^=fa-edit_]")));
+            Thread.Sleep(2000);
+            deliveriesOld[0].Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("swal-input1")).SendKeys("1");
+            Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector(".swal2-confirm")).Click();
+            wait.Until(e => e.FindElement(By.CssSelector(".swal2-confirm"))).Click();
+            Thread.Sleep(2000);
+            IList<IWebElement> operatorsNew = driver.FindElements(By.CssSelector("[id^=operator_]"));
+            // Assert.AreEqual(deliveries.Count + 1, operatorsNew.Count);
+            Thread.Sleep(2000);
+            driver.Quit();
         }
     }
 }
