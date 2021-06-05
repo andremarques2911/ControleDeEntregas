e3b65e9fabe06f684f0579aaf530056fc5530047
André <Marques>
2021-06-05 04:17:10 -0300

    Adicionado frontend

---
+++ web_driver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace Login
+{
+    class Program
+    {
+        const string BROWSER = "CHROME";
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"chrome\chromedriver");
+
+            driver.Navigate().GoToUrl("http://localhost:3000/");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement firstResult = wait.Until(e => e.FindElement(By.Id("main_operators")));
+            firstResult.Click();
+            var operatorsOld = driver.FindElements(By.CssSelector("[id^=operator_]"));
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("first_name")).SendKeys("Gui");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("last_name")).SendKeys("Carvalho");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("save_operator")).Click();
+            Thread.Sleep(2000);
+            // swal2-title
+            // Thread.Sleep(1000);
+            driver.FindElement(By.CssSelector(".swal2-confirm")).Click();
+            // driver.FindElement(By.Id("swal2-confirm")).Click();
+            Thread.Sleep(2000);
+            var operatorsNew = driver.FindElements(By.CssSelector("[id^=operator_]"));
+            
+
+            // driver.Quit();
+        }
+    }
+}
---
+++ web_driver/WebDriver.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class BuscaGoogleTest
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+    }
+    [TestMethod]
+    public void BuscarSeleniuWebDriver_PrimeiroResultadoEnderecoSiteSelenium()
+    {
+    }
+    [TestCleanup]
+    public void CleanUp()
+    {
+    }
+}
